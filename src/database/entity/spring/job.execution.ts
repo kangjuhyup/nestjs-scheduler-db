7f133902e94a3a67a8cd755e4fdb3c09b5573af4
kangjuhyup <fog0510@gmail.com>
2024-09-28 16:37:32 +0900

    chore : entity 수정

--- src/database/entity/spring/job.execution.ts
+++ src/database/entity/spring/job.execution.ts
@@ -4,17 +4,23 @@ import {
   Column,
   ManyToOne,
   JoinColumn,
+  UpdateDateColumn,
+  CreateDateColumn,
 } from 'typeorm';
 import { BatchJobInstance } from './job.instance';
+import { BatchStatus, ExistStatus } from './enum';
 
 @Entity('BATCH_JOB_EXECUTION')
 export class BatchJobExecution {
   @PrimaryGeneratedColumn()
   JOB_EXECUTION_ID: number;
 
+  @Column({ type: 'bigint' })
+  JOB_INSTANCE_ID: number;
+
   @ManyToOne(() => BatchJobInstance)
   @JoinColumn({ name: 'JOB_INSTANCE_ID' })
-  JOB_INSTANCE_ID: BatchJobInstance;
+  JOB_INSTANCE: BatchJobInstance;
 
   @Column({ type: 'timestamp' })
   CREATE_TIME: Date;
@@ -26,15 +32,15 @@ export class BatchJobExecution {
   END_TIME: Date;
 
   @Column({ type: 'varchar', length: 10 })
-  STATUS: string;
+  STATUS: BatchStatus;
 
   @Column({ type: 'varchar', length: 2500, nullable: true })
-  EXIT_CODE: string;
+  EXIT_CODE: ExistStatus;
 
   @Column({ type: 'varchar', length: 2500, nullable: true })
   EXIT_MESSAGE: string;
 
-  @Column({ type: 'timestamp', nullable: true })
+  @UpdateDateColumn({ type: 'timestamp', nullable: true })
   LAST_UPDATED: Date;
 
   @Column({ type: 'bigint' })
