99ee602bebd3b99675569f85fb413f94cd6f6356
kangjuhyup <fog0510@gmail.com>
2024-09-28 15:19:17 +0900

    chore : CronWithDB job 전달받도록 수정

--- src/scheduler/decorator.ts
+++ src/scheduler/decorator.ts
@@ -2,14 +2,21 @@ import { Cron, CronExpression } from '@nestjs/schedule';
 import { applyDecorators, SetMetadata } from '@nestjs/common';
 
 export const CRON_WITH_DB = 'CRON_WITH_DB';
+export const CRON_JOB_NAME = 'CRON_JAB_JNAME';
 export const CRON_STEP = 'CRON_STEP';
 
 /**
  * @description Cron 데코레이터 확장
  * @param cronTime
  */
-export function CronWithDB(cronTime: string = CronExpression.EVERY_MINUTE) {
-  return applyDecorators(Cron(cronTime), SetMetadata(CRON_WITH_DB, true));
+export function CronWithDB(param: { jobName: string; cronTime: string }) {
+  if (!param.jobName) throw new Error('배치 Job 이름이 없습니다.');
+  if (!param.cronTime) throw new Error('크론 시간 표현식이 없습니다.');
+  return applyDecorators(
+    Cron(param.cronTime),
+    SetMetadata(CRON_WITH_DB, true),
+    SetMetadata(CRON_JOB_NAME, param.jobName),
+  );
 }
 
 /**
